f4b45089adaeb01177f058ea779640f94dc3b2d2
Flavio-VM-Nitro5 <flaviovmix@gmail.com>
2024-07-04 16:31:33 -0300

    CLASSE FILME FUNCONANDO CORRETAMENTE

--- src/Filme.java
+++ src/Filme.java
@@ -2,7 +2,26 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacaco;
-    int totalDeAvaliacoes;
+    private double somaDasAvaliacaco;
+    private int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do Filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        System.out.println("Duração em minutos: " + duracaoEmMinutos);
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacaco += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia() {
+        return somaDasAvaliacaco/totalDeAvaliacoes;
+    }
 }
---
+++ src/Principal.java
@@ -0,0 +1,18 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O Poderoso Chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        meuFilme.exibeFichaTecnica();
+
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+
+        System.out.println(meuFilme.pegaMedia());
+    }
+}
