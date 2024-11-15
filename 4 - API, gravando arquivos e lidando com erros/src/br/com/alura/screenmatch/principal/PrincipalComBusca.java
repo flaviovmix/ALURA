a1fbf539fe0a4ea01d7fa928a121cf48e7ca1b64
Flavio <flaviovmix@gmail.com>
2024-11-15 18:54:14 -0300

    241115

---
+++ 4 - API, gravando arquivos e lidando com erros/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,35 @@
+package br.com.alura.screenmatch.principal;
+import br.com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.print("Digite um filme para busca: ");
+        String busca = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=13cdfd85";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo meutitulo = gson.fromJson(json, Titulo.class);
+
+        System.out.println(meutitulo);
+    }
+}
